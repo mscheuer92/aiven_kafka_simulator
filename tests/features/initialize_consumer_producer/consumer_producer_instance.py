90f70113b9282a78fd38b273c7f12a6001c0509e
mscheuer92 <michelle.scheuer@wexinc.com>
2022-06-30 16:48:20 -0400

    Add auto_offset_reset to latest to the consumer.

--- tests/features/initialize_consumer_producer/consumer_producer_instance.py
+++ tests/features/initialize_consumer_producer/consumer_producer_instance.py
@@ -7,7 +7,8 @@ consumer = kafka.KafkaConsumer(group_id='payee-group',
                                security_protocol=constant.SECURITY_PROTOCOL,
                                ssl_cafile=constant.SSL_CAFILE,
                                ssl_certfile=constant.SSL_CERTFILE,
-                               ssl_keyfile=constant.SSL_KEYFILE, )
+                               ssl_keyfile=constant.SSL_KEYFILE,
+                               auto_offset_reset="latest")
 
 producer = kafka.KafkaProducer(bootstrap_servers=constant.BOOTSTRAP_SERVER,
                                security_protocol=constant.SECURITY_PROTOCOL,
